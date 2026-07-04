96b8b8b09c12b2e86fa04bdd9c6375b8b445bc81
Yanbo Ding <dingyanb@narval1.narval.calcul.quebec>
2026-07-04 11:01:44 -0400

    finished running myopic and alp policy

--- param_generation/cli.py
+++ param_generation/cli.py
@@ -182,13 +182,13 @@ def recipe_case_study_099_mixture_geometric_proposal_095_policy_evaluation():
     generate_test_paths_and_init_state(
         test_envs=test_envs,
         test_sample_path_num=1000,
-        warm_up_periods=0,
-        num_periods=None,
+        warm_up_periods=1500,
+        num_periods=1500,
         dat_file='table.dat',
         num_groups=500,  # divide into N groups
-        is_require_penalty_coefficients=True,
+        is_require_penalty_coefficients=False,
         is_random_initial_state=True,
-        policy_ids=['approx_penalized_hindsight', 'row_gen_alp', 'myopic'],
+        policy_ids=['row_gen_alp', 'myopic'],
     )
 
 def main():
